789265e18cae4c711b67433508214e1a94b51ed4
Nike0000 <Nike0000@PCB-LAB360-25>
2019-10-15 11:09:30 -0500

    Function Participation

---
+++ CollectionParticipation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionParticipation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> gradelist = new List<double>();
+
+            string answer;
+            
+                
+              do
+            {
+                Console.WriteLine("Please enter your exam score. >>");
+                string gradeasstring = Console.ReadLine();
+                double grade;
+
+               gradelist.Add(grade = 0);
+
+
+                Console.WriteLine("Do you have another exam grade to enter? (Yes or No) >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            
+
+
+            foreach (double grade in gradelist)
+            {
+                double maxgrade = gradelist[0];
+
+                if (grade > maxgrade)
+                {
+                    maxgrade = grade;
+                }
+
+                Console.WriteLine($"Your maximum exam grade is {maxgrade}");
+            }
+
+           
+
+           
+            
+
+            Console.ReadKey();
+           
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string msg = "";
+            GetDoubleInputFromUser(msg);
+
+            Console.ReadKey();
+        }
+
+                static double GetDoubleInputFromUser(string msg)
+                {
+                    double num;
+                    double result = 0;
+            
+
+                    while (double.TryParse(msg, out num) == false)
+                    {
+                        Console.WriteLine("Please input a number. >>");
+                        msg = (Console.ReadLine());
+
+                        result = 0;
+                        if (double.TryParse(msg, out num))
+                        {
+                            result = num * 5;
+
+
+                            Console.WriteLine($"{num} * 5 = {result}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry you did not input a valid number, please try again.");
+                        }
+                
+                    }
+                    return result;
+                }
+            
+        
+    }
+}
